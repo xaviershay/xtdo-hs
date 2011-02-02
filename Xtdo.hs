607367578794be66cf2a684eccaf4445455c2cc7
Xavier Shay <xavier@rhnh.net>
2011-02-03 07:51:54 +1100

    Use $ operator now that I know what it does

--- Xtdo.hs
+++ Xtdo.hs
@@ -6,7 +6,7 @@ import Data.List
 main :: IO ()
 main = do
   args <- getArgs
-  finish (xtdo args [Task{ name="do something", scheduled=Nothing, category=Today}])
+  finish $ xtdo args [Task{ name="do something", scheduled=Nothing, category=Today}]
 
 data TaskCategory = Today | Next | Scheduled deriving(Show, Eq)
 data Task = Task { name :: String, scheduled :: Maybe Day, category :: TaskCategory } deriving(Show)
@@ -16,16 +16,16 @@ xtdo ["l"]      tasks = (tasks, [Today])
 xtdo ["l", "a"] tasks = (tasks, [Today, Next])
 
 finish (tasks, categoriesToDisplay) = 
-  putStrLn (intercalate "\n" output) 
+  putStrLn $ intercalate "\n" output
   where output = flatten [[formatCategory c] ++ [formatTask t | t <- tasks, category t == c] | c <- categoriesToDisplay]
 
 flatten = foldl (++) [] -- Surely this is in the stdlib?
 
 formatCategory :: TaskCategory -> String
-formatCategory x = "==== " ++ show(x) ++ "\n"
+formatCategory x = "==== " ++ show x ++ "\n"
 
 formatTask :: Task -> String
-formatTask x = "  " ++ name(x)
+formatTask x = "  " ++ name x
 
 -- 
 --
