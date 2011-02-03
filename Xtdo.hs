6c167f5c6b8d07e4ba38605b138ead648d63a1b2
Xavier Shay <xavier@rhnh.net>
2011-02-03 20:08:02 +1100

    Got me some yaml yeah

--- Xtdo.hs
+++ Xtdo.hs
@@ -2,11 +2,16 @@ import System.Environment
 import Data.Time.Calendar
 import Data.List
 
+import Data.Object
+import Data.Object.Yaml
+import Control.Monad
+
 -- 'main' runs the main program
 main :: IO ()
 main = do
   args <- getArgs
-  finish $ xtdo args [Task{ name="do something", scheduled=Nothing, category=Today}]
+  tasks <- loadYaml
+  finish $ xtdo args tasks
 
 data TaskCategory = Today | Next | Scheduled deriving(Show, Eq)
 data Task = Task { name :: String, scheduled :: Maybe Day, category :: TaskCategory } deriving(Show)
@@ -15,21 +20,34 @@ xtdo :: [String] -> [Task] -> ([Task], [TaskCategory])
 xtdo ["l"]      tasks = (tasks, [Today])
 xtdo ["l", "a"] tasks = (tasks, [Today, Next])
 
-finish (tasks, categoriesToDisplay) = 
-  putStrLn $ intercalate "\n" output
+finish (tasks, categoriesToDisplay) = do
+  encodeFile "tasks.yml" $ Sequence $ map toYaml tasks
+  putStrLn $ intercalate "\n" output ++ "\n"
   where output = flatten [ [formatCategory c] ++ 
-                           [formatTask t | t <- tasks, category t == c] ++ 
-                           [""]
+                           [formatTask t | t <- tasks, category t == c]
                          | c <- categoriesToDisplay]
 
 flatten = foldl (++) [] -- Surely this is in the stdlib?
 
 formatCategory :: TaskCategory -> String
-formatCategory x = "==== " ++ show x ++ "\n"
+formatCategory x = "\n==== " ++ show x ++ "\n"
 
 formatTask :: Task -> String
 formatTask x = "  " ++ name x
 
+toYaml Task{name=x, scheduled=Nothing} = Mapping [("name", Scalar x)]
+
+loadYaml = do
+  object <- join $ decodeFile "tasks.yml"
+  tasks <- fromSequence object >>= mapM extractTask
+  return tasks
+
+extractTask task = do
+  m <- fromMapping task
+  n <- lookupScalar "name" m
+  return Task{name=n, scheduled=Nothing, category=Today}
+
+testTasks = [Task{ name="do something", scheduled=Nothing, category=Today}]
 -- 
 --
 -- Each command returns:
