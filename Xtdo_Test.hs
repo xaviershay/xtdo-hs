462d4d6395d243041f6b8f20fdc499a411a5536c
Xavier Shay <xavier@rhnh.net>
2011-06-26 09:18:51 +1000

    Reformat tests to make them more readable.

--- Xtdo_Test.hs
+++ Xtdo_Test.hs
@@ -9,21 +9,25 @@ import Data.Time.Calendar
 d = fromGregorian
 
 xtdoTests =
-  [ "l only shows Today" ~: 
+  [ "l only shows Today" ~:
     (tasks, [Today]) ~=? (xtdo ["l"] tasks today)
+
   , "l a shows all tasks" ~:
     (tasks, [Today, Next, Scheduled]) ~=? (xtdo ["l", "a"] tasks today)
+
   , "d shows only today and next" ~:
     [Today, Next] ~=? (snd $ xtdo ["d", "mytask"] tasks today)
+
   , "d removes any task that matches the given name" ~:
     [chaff] ~=? (fst $ xtdo["d", "mytask"] tasks today)
   ]
-  where tasks = 
+  where tasks =
           [ Task{name="mytask", scheduled=Nothing, category=Next}
           , chaff
           ]
         chaff = Task{name="chaff",  scheduled=Nothing, category=Next}
         today = (d 2011 1 1)
+
 dayTests =
   [ t "1d"  (d 2011 2 2)
   , t "2d"  (d 2011 2 3)
@@ -32,6 +36,8 @@ dayTests =
   , t "1m"  (d 2011 3 1)
   , t "1y"  (d 2012 2 1)
   ]
-  where t str expected = "Xtdo.day parses " ++ str ~: expected ~=? (day (d 2011 2 1) str)
+  where t str expected = "Xtdo.day parses " ++ str ~:
+                            expected ~=? (day today str)
+        today          = (d 2011 2 1)
 
 main = runTestTT $ TestList ( xtdoTests ++ dayTests )
